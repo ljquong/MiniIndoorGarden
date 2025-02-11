d1f63adc84066734b26bd63208ab6c81a5a8e465
ljquong <132942951+ljquong@users.noreply.github.com>
2025-02-11 10:35:54 -0700

    Add file via upload
    Basic functionality of photocell, float switch, and water level LED.

---
+++ lightControls.py
@@ -0,0 +1,63 @@
+from machine import Pin, PWM
+import time
+
+ldr = machine.ADC(28)
+grow_lights = Pin(16, Pin.OUT)
+
+# Initialize PWM pins for each color of the RGB LED
+red = PWM(Pin(11, Pin.OUT))
+green = PWM(Pin(12, Pin.OUT))
+blue = PWM(Pin(13, Pin.OUT))
+
+float_switch = Pin(10, Pin.IN, Pin.PULL_DOWN)
+
+# Set all PWM pins to 1000 Hz frequency
+red.freq(1000)
+blue.freq(1000)
+green.freq(1000)
+
+# Function to set RGB LED color by converting 8-bit integers for
+# r (red), g (green), b (blue) into 16-bit integers, that are
+# used to set the duty cycles for the appropriate PWM pin
+def set_color(r, g, b):
+    red.duty_u16(r * 257)
+    green.duty_u16(g * 257)
+    blue.duty_u16(b * 257)
+    
+set_color(0, 0, 0)
+
+#remove print statements here
+#function to check ldr value to see if it is 'dark' for 10 seconds
+#if it is, turn on the led lights, if it is 'not dark' for 10 seconds, turn lights off
+def read_photocell():
+    for i in range(10):
+        ldr_value = ldr.read_u16()
+        is_dark = False
+        if ldr_value > 60000:
+            is_dark = True
+        else:
+            is_dark = False
+        time.sleep(1)
+    if is_dark:
+        #grow_lights.value(1)
+        print("It is DARK.")
+    else:
+        #grow_lights.value(0)
+        print("It is BRIGHT.")
+
+#read the state of the float switch: 0 means liquid present, so light should not turn on
+#if there is no water, the light should turn on
+def read_float_switch():
+    if float_switch.value() == 0:
+        set_color(0, 0, 0)
+        print("There is WATER.")
+    else:
+        set_color(255, 0, 0)
+        print("There is NO WATER.")
+
+#main program to run both functions continuously
+while True:
+    read_photocell()
+    read_float_switch()
+    print()
+    time.sleep(1)
