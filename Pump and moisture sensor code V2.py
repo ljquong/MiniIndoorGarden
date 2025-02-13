61d0badc6cfa66181aa655cefed2ec119e473530
Inbss <inbal.sharlin@gmail.com>
2025-02-13 12:37:46 -0700

    Update Pump and moisture sensor code V2.py

--- Pump and moisture sensor code V2.py
+++ Pump and moisture sensor code V2.py
@@ -12,35 +12,42 @@ seesaw = stemma_soil_sensor # Data Sheet = ~200 (very dry) to ~2000 (very wet) 1
                             # Soil sensor has temp + or - 2 degrees Celsius
 pump = Pin(16, Pin.OUT) # Pump pin is connected to the transistor base, Pump is connected to 5v of PI Pico with a potentiometer in series to control flow.
 
+def moisture_read():
+  moisture = seesaw.get_moisture()
+  temperature = seesaw.get_temp()
+  print(f'Moisture Level: {moisture}, Temperature: {temperature:.1f}{chr(176)}C')
+  time.sleep(2)
+  if moisture < 415:
+      if moisture >= 400 and moisture < 415:
+          num_pump = 3
+          print("REGULAR WATERING IN PROGRESS")
+      elif moisture >= 370 and moisture < 400:
+          num_pump = 5
+          print("SLIGHTLY DRY: WATERING IN PROGRESS")
+      elif moisture >= 340 and moisture < 270:
+          print("DRY: WATERING IN PROGRESS")
+          num_pump = 7
+      else:
+          print("VERY DRY: WATERING IN PROGRESS")
+          num_pump = 9
+    return num_pump
+
+def pumping(num_pump):
+  if num_pump > 0:
+      for i in range(num_pump):
+          pump.value(1) # turn pump on
+          time.sleep(10) # pump will remain on for 10s
+          
+          pump.value(0) # turn pump off
+          time.sleep(3) # pump will remain off for 10s
+    print("<<Watering complete!>>\n")
+    time.sleep(20) #1-2 minutes in final design 
+
 while True:
     try:
-        num_pump = 0
-        moisture = seesaw.get_moisture()
-        temperature = seesaw.get_temp()
-        print(f'Moisture Level: {moisture}, Temperature: {temperature:.1f}{chr(176)}C')
-        time.sleep(2)
-        if moisture < 415:
-            if moisture >= 400 and moisture < 415:
-                num_pump = 3
-                print("REGULAR WATERING IN PROGRESS")
-            elif moisture >= 370 and moisture < 400:
-                num_pump = 5
-                print("SLIGHTLY DRY: WATERING IN PROGRESS")
-            elif moisture >= 340 and moisture < 270:
-                print("DRY: WATERING IN PROGRESS")
-                num_pump = 7
-            else:
-                print("VERY DRY: WATERING IN PROGRESS")
-                num_pump = 9
-        if num_pump > 0:
-            for i in range(num_pump):
-                pump.value(1) # turn pump on
-                time.sleep(10) # pump will remain on for 10s
-                
-                pump.value(0) # turn pump off
-                time.sleep(3) # pump will remain off for 10s
-            print("<<Watering complete!>>\n")
-        time.sleep(20) #1-2 minutes in final design      
+        times_pump = moisture_read()
+        if times_pump > 0:
+          pumping(times_pump)
             
          
     except Exception as e:
